66613982e17dd72f6186869818beec09cd061e99
Lawrence Wang <lwang801118@gmail.com>
2023-06-30 19:03:31 +0000

    entries list

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,25 @@ import './reset.css';
 import './layout.css';
 import './styles.css';
 
+/* exported data */
+
+let data = {
+  view: 'entry-form',
+  entries: [],
+  editing: null,
+  nextEntryId: 1,
+};
+
+window.addEventListener('beforeunload', function (event) {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal-data', dataJSON);
+});
+
+const localData = JSON.parse(localStorage.getItem('code-journal-data'));
+if (localData) {
+  data = localData;
+}
+
 function App() {
   return (
     <div className="App">
